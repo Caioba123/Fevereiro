2893a00102f72a5ac187dc66ace5b4949a24c2f9
Caio Müller <caiomuller7192@gmail.com>
2026-02-11 09:16:32 -0300

    Add files via upload

---
+++ 11.02/tela01.js
@@ -0,0 +1,19 @@
+function  Tela01(){ 
+    return(
+
+        <>
+
+<div className="Container">
+<h5>Hoje tem náutico</h5>
+<h1>Neymar</h1>
+<h2>Cr7</h2>
+<h3>Messi</h3>
+<h4>Dodô</h4>
+
+</div>
+
+        </>
+
+    )
+}
+export default Tela01;
