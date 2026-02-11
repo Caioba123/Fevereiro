1e8e2d32969cac998c702f22ba890f58b0e2df24
Caio Müller <caiomuller7192@gmail.com>
2026-02-11 09:13:24 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './views/teste.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Campeonato Pernambucano</h1>
+  
+    </div>
+  );
+}
+
+export default App;
