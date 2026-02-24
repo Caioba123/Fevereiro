e89be82848477fce392cc37b2f4f355cdc133f1a
Caio Müller <caiomuller7192@gmail.com>
2026-02-23 21:13:50 -0300

    Add files via upload

---
+++ 19.02/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>🚀 Projeto React Base Funcionando!</h1>
+      <p>Agora você pode começar a desenvolver.</p>
+    </div>
+  )
+}
+
+export default App
